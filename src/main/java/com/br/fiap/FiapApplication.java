2ac1a280edd552ddff9b90cde832a05fe92211b8
gustavoreche <gustavodreche@msn.com>
2023-12-31 16:50:05 -0300

    Habilita config do FeignClient

--- src/main/java/com/br/fiap/FiapApplication.java
+++ src/main/java/com/br/fiap/FiapApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import com.br.fiap.cliente.ClienteFront;
 import com.br.fiap.vendedor.VendedorFront;
 
+@EnableFeignClients
 @SpringBootApplication
 public class FiapApplication implements CommandLineRunner {
 	
